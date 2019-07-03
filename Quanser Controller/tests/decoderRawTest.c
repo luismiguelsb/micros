d4e3c11322f59fa6a8eebe21c50ba8977cff668d
DESKTOP-8N3586S\Melkor <nicolas.eymael@hotmail.com>
2019-07-03 19:03:38 -0300

    decoderRawTest

--- Quanser Controller/tests/decoderRawTest.c
+++ Quanser Controller/tests/decoderRawTest.c
@@ -5,5 +5,22 @@
 
 int main(int argc,char * argv[])
 {
-	// do something
+	int result;
+	int choice;
+	
+	setupDecoder();
+	result = readDecoder(READ_CNTR);
+	
+	printf("primeira leitura (teste) = %d\n", result);
+	
+	printf("deseja permanecer lendo? (1-sim, 0-não)\n");
+	fscanf("%d", &choice);
+	
+	if(choice == 1)
+	{
+		result = readDecoder(READ_CNTR);
+		printf("decoder result = %d\n", result);
+	}
+	
+	return 0;
 }
