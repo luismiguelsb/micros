c5912e3e08832295e3d8145fa73beb2f277a2eef
ENG10032 Microcontroladores - Grupo 19 <eng10032_19@barbalat.ece>
2019-07-10 09:00:57 -0300

    zip atualizado

--- Quanser Controller/trabsv2/codigo/tests/decoderRawTest.c
+++ Quanser Controller/trabsv2/codigo/tests/decoderRawTest.c
@@ -6,16 +6,13 @@
 int main(int argc,char * argv[])
 {
 	int result;
-	int result2;
 	
 	setupDecoder();
 	
 	while(1)
 	{
 		result = readDecoder();
-		result2 = readCounterLauren();
 		printf("decoder raw = %d\t", result);
-		printf("decoder raw = %d\n", result2);
 	}
 	
 	return OK;
